582a71e0a7b6b5113917c03572d9dd91822bd0e6
Emilio Palumbo <emiliopalumbo@gmail.com>
2018-10-22 18:15:19 +0200

    Update test script with long options

--- scripts/test.sh
+++ scripts/test.sh
@@ -117,7 +117,7 @@ log "Quantifications"
 (mkdir txIndex && rsem-prepare-reference --gtf /rnaseq/refs/anno.gtf \
                       /rnaseq/refs/genome.fa \
                       txIndex/RSEMref) &> error.log
-(mkdir quantifications && rsem-calculate-expression -p 2 \
+(mkdir quantifications && rsem-calculate-expression --num-threads 2 \
                           --bam \
                           --paired-end \
                           --estimate-rspd \
@@ -127,8 +127,18 @@ log "Quantifications"
                           alignments/mouse_cns_E18_rep1_Aligned.toTranscriptome.out.bam \
                           txIndex/RSEMref \
                           quantifications/mouse_cns_E18_rep1) &> error.log
-(cat $RNASEQ/data/quantifications.index.txt | retrieve_element_rpkms.py -o encode -O mouse -e gene -v FPKM -d quantifications) 2> error.log
-(cat $RNASEQ/data/quantifications.index.txt | retrieve_element_rpkms.py -o encode -O mouse -e gene -v expected_count -d quantifications) 2> error.log
+(cat $RNASEQ/data/quantifications.index.txt \
+| retrieve_element_rpkms.py --output encode \
+                            --organism mouse \
+                            --element gene \
+                            --value FPKM \
+                            --output_dir quantifications) 2> error.log
+(cat $RNASEQ/data/quantifications.index.txt \
+| retrieve_element_rpkms.py --output encode \
+                            --organism mouse \
+                            --element gene \
+                            --value expected_count \
+                            --output_dir quantifications) 2> error.log
 logOK
 
 ##########
@@ -137,40 +147,41 @@ logOK
 mkdir -p analysis
 # RPKM distribution
 log "RPKM distribution"
-rpkm_distribution.R -i quantifications/encode.mouse.gene.FPKM.idr_NA.tsv \
-                    -l -p 0 -m $RNASEQ/data/quantifications.index.tsv -f age \
-                    --palette=$RNASEQ/palettes/cbbPalette.8.txt -d analysis &> error.log
+rpkm_distribution.R --input_matrix quantifications/encode.mouse.gene.FPKM.idr_NA.tsv \
+                    --log --pseudocount 0 --metadata $RNASEQ/data/quantifications.index.tsv \
+                    --fill_by age --palette $RNASEQ/palettes/cbbPalette.8.txt \
+                    --outdir analysis &> error.log
 logOK
 
 # Clustering
 log "Clustering"
-matrix_to_dist.R -i quantifications/encode.mouse.gene.FPKM.idr_NA.tsv --log10 \
-                 -c pearson -o stdout | ggheatmap.R -i stdin \
+matrix_to_dist.R --input_matrix quantifications/encode.mouse.gene.FPKM.idr_NA.tsv --log10 \
+                 --cor pearson --output stdout | ggheatmap.R --input_matrix stdin \
                  --row_metadata $RNASEQ/data/quantifications.index.tsv \
-                 --col_dendro --row_dendro -B 10 \
-                 --matrix_palette=$RNASEQ/palettes/terrain.colors.3.txt --rowSide_by age \
-                 --matrix_fill_limits 0.85,1 -o analysis/cns.heatmap.pdf &> error.log
+                 --col_dendro --row_dendro --base_size 10 \
+                 --matrix_palette $RNASEQ/palettes/terrain.colors.3.txt --rowSide_by age \
+                 --matrix_fill_limits 0.85,1 --output analysis/cns.heatmap.pdf &> error.log
 logOK
 
 # DGE analysis
 log "Differential gene expression"
-edgeR.analysis.R -i quantifications/encode.mouse.gene.expected_count.idr_NA.tsv \
-                 -m $RNASEQ/data/quantifications.index.tsv -f age -d analysis &> error.log
+edgeR.analysis.R --input_matrix quantifications/encode.mouse.gene.expected_count.idr_NA.tsv \
+                 --metadata $RNASEQ/data/quantifications.index.tsv --fields age --output_dir analysis &> error.log
 (awk '$NF<0.01 && $4>2{print $1"\tover18"}' analysis/edgeR.cpm1.n2.out.tsv \
     > analysis/edgeR.0.01.overE18.txt) 2> error.log
 (awk '$NF<0.01 && $4<-2 {print $1"\tover14"}' analysis/edgeR.cpm1.n2.out.tsv \
     > analysis/edgeR.0.01.overE14.txt) 2> error.log
 [ $((wc -l analysis/edgeR.0.01.overE14.txt | cut -d' ' -f1) 2> error.log) -eq 79 ]
 [ $((wc -l analysis/edgeR.0.01.overE18.txt | cut -d' ' -f1) 2> error.log) -eq 82 ]
-(join.py -a <((zcat $RNASEQ/refs/mm65.long.ok.gtf.gz | awk 'BEGIN{OFS="\t"}$3=="gene"{ match($0, /gene_id "([^"]+).+gene_type "([^"]+)/, var); print var[1],var[2]  }') 2> error.log) \
-        -b <(cat analysis/edgeR.0.01.over*.txt 2> error.log) | sed '1igene\tedgeR\tgene_type' \
-        > analysis/gene.edgeR.tsv) 2> error.log
+(join.py --file1 <((zcat $RNASEQ/refs/mm65.long.ok.gtf.gz | awk 'BEGIN{OFS="\t"}$3=="gene"{ match($0, /gene_id "([^"]+).+gene_type "([^"]+)/, var); print var[1],var[2]  }') 2> error.log) \
+         --file2 <(cat analysis/edgeR.0.01.over*.txt 2> error.log) | sed '1igene\tedgeR\tgene_type' \
+         > analysis/gene.edgeR.tsv) 2> error.log
 (cut -f1 analysis/gene.edgeR.tsv | tail -n+2 | selectMatrixRows.sh - \
-    quantifications/encode.mouse.gene.FPKM.idr_NA.tsv | ggheatmap.R -W 5 -H 9 \
+    quantifications/encode.mouse.gene.FPKM.idr_NA.tsv | ggheatmap.R --width 5 --height 9 \
     --col_metadata $RNASEQ/data/quantifications.index.tsv --colSide_by age \
     --col_labels labExpId --row_metadata analysis/gene.edgeR.tsv --merge_row_mdata_on gene \
-    --rowSide_by edgeR,gene_type --row_labels none -l -p 0.1 --col_dendro --row_dendro \
-    --matrix_palette=$RNASEQ/palettes/terrain.colors.3.txt -o analysis/heatmap.edgeR.pdf) \
+    --rowSide_by edgeR,gene_type --row_labels none --log --pseudocount 0.1 --col_dendro --row_dendro \
+    --matrix_palette $RNASEQ/palettes/terrain.colors.3.txt --output analysis/heatmap.edgeR.pdf) \
     &> error.log
 logOK
 
@@ -178,12 +189,12 @@ logOK
 log "GO enrichment"
 (zcat $RNASEQ/refs/mm65.long.ok.gtf.gz | awk '{split($10,a,"\""); print a[2]}' | sort -u \
     > analysis/universe.txt) 2> error.log
-(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R -u analysis/universe.txt \
-    -G stdin -c BP -o analysis/edgeR.overE14 -s mouse) 2> error.log
-(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R -u analysis/universe.txt \
-    -G stdin -c MF -o analysis/edgeR.overE14 -s mouse) 2> error.log
-(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R -u analysis/universe.txt \
-    -G stdin -c CC -o analysis/edgeR.overE14 -s mouse) 2> error.log
+(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R --universe analysis/universe.txt \
+    --genes stdin --categ BP --output analysis/edgeR.overE14 --species mouse) 2> error.log
+(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R --universe analysis/universe.txt \
+    --genes stdin --categ MF --output analysis/edgeR.overE14 --species mouse) 2> error.log
+(cut -f1 analysis/edgeR.0.01.overE14.txt | GO_enrichment.R --universe analysis/universe.txt \
+    --genes stdin --categ CC --output analysis/edgeR.overE14 --species mouse) 2> error.log
 logOK
 
 # Validate md5 checksums
